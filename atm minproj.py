5bc0eaa50e96b5f819bed5c3b626f5820f6e2eb8
GH Jayanthi <jayantijayanti9381@gmail.com>
2025-03-17 18:00:16 +0530

    Add files via upload

---
+++ atm minproj.py
@@ -0,0 +1,40 @@
+balance=500
+def deposit(amount):
+    global balance
+    balance+=amount
+    print(f"deposited balance:RS{balance}")
+
+
+def withdraw(amount):
+    global balance
+    if balance>=amount:
+        balance-=amount
+        print(f"with draw amount is {amount}")
+        print(f"after withdraw of amount is {balance}")
+    else:
+        print("insufficent balance")
+
+def balance_enquary(amount):
+    print(f"available balance in your account is :{balance}")
+print(balance_enquary)
+
+while True:
+    print("------------ATM menu------------")
+    print("1.deposit")
+    print("2.eithdraw")
+    print("3.balance_enquary")
+    print("4.exit")
+    choice=input("enter  your choice:")
+    if choice=="1":
+        amount=int(input("enter the amount deposit your account"))
+        deposit(amount)
+    elif choice=="2":
+        amount=int(input("enter the withdraw amount"))
+        withdraw(amount)
+    elif choice=="3":
+        #amount=int(input("the remaing balance is"))
+        balance_enquary()
+    elif choice=="4":
+        print("thank you vist again")
+    else:
+        print("invalid choice,choose the coorect answer")
